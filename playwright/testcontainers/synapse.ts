8fb02a461753ca8fd2be7e05546b4ed38d00c66b
John Evans <38507954+King-Mob@users.noreply.github.com>
2026-09-24 16:43:30 +0100

    init

---
+++ playwright/testcontainers/synapse.ts
@@ -0,0 +1,21 @@
+/*
+Copyright 2024-2025 New Vector Ltd.
+
+SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Commercial
+Please see LICENSE files in the repository root for full details.
+*/
+
+import { SynapseContainer as BaseSynapseContainer } from "@element-hq/element-web-playwright-common/lib/testcontainers/index.js";
+
+const DOCKER_IMAGE =
+    "ghcr.io/element-hq/synapse:develop@sha256:7006b6aaa6285355e45d7639139b337fe76ccaf24d0791bb31c1ac4e2bce53fb";
+
+/**
+ * SynapseContainer which freezes the docker digest to stabilise tests,
+ * updated periodically by the `playwright-image-updates.yaml` workflow.
+ */
+export class SynapseContainer extends BaseSynapseContainer {
+    public constructor() {
+        super(DOCKER_IMAGE);
+    }
+}
